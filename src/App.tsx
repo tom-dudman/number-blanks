3b6e76689c7ddfd54e5e7a2857d7d99371a94a1f
Tom Dudman <tomdudman@gmail.com>
2025-04-27 21:10:03 +0100

    set reveal to false upon settings change

--- src/App.tsx
+++ src/App.tsx
@@ -61,6 +61,7 @@ function App() {
       difficulty,
     });
     setModes(newModes);
+    setReveal(false);
   };
 
   const handleCommitDifficulty = ([newDifficulty]: number[]) => {
@@ -69,6 +70,7 @@ function App() {
       difficulty: newDifficulty,
     });
     setDifficulty(newDifficulty);
+    setReveal(false);
   };
 
   const mode = chooseOperation(modes);
